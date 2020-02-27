ea3e440eda4309cc25484a0bdd099a26e98ffa33
rumtid <rumtid@users.noreply.github.com>
2020-02-27 13:52:24 +0800

    Answer Q51 & Q52

---
+++ 0051. N-Queens/0051_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func Example_case1() {
+	ans := solveNQueens(4)
+	for _, row := range ans {
+		fmt.Println(row)
+	}
+	// Unordered output:
+	// [.Q.. ...Q Q... ..Q.]
+	// [..Q. Q... ...Q .Q..]
+}
---
+++ 0052. N-Queens II/0052_test.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func Example_case1() {
+	ans := totalNQueens(4)
+	fmt.Print(ans)
+	// Output:
+	// 2
+}
