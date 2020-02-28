71ed00d221764a3fa73a1bb86662a161a1e7bf4e
rumtid <rumtid@users.noreply.github.com>
2020-02-28 13:06:31 +0800

    Answer Q53 & Q54

---
+++ 0053. Maximum Subarray/0053.go
@@ -0,0 +1,18 @@
+package main
+
+func maxSubArray(nums []int) int {
+	pre, sum := nums[0], nums[0]
+	for _, num := range nums[1:] {
+		pre = max(pre+num, num)
+		sum = max(sum, pre)
+	}
+	return sum
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
---
+++ 0054. Spiral Matrix/0054.go
@@ -0,0 +1,37 @@
+package main
+
+func spiralOrder(matrix [][]int) []int {
+	if len(matrix) == 0 {
+		return []int{}
+	}
+	var list []int
+	ilo, ihi := 0, len(matrix)-1
+	jlo, jhi := 0, len(matrix[0])-1
+	for {
+		for k := jlo; k <= jhi; k++ {
+			list = append(list, matrix[ilo][k])
+		}
+		if ilo++; ilo > ihi {
+			break
+		}
+		for k := ilo; k <= ihi; k++ {
+			list = append(list, matrix[k][jhi])
+		}
+		if jhi--; jhi < jlo {
+			break
+		}
+		for k := jhi; k >= jlo; k-- {
+			list = append(list, matrix[ihi][k])
+		}
+		if ihi--; ihi < ilo {
+			break
+		}
+		for k := ihi; k >= ilo; k-- {
+			list = append(list, matrix[k][jlo])
+		}
+		if jlo++; jlo > jhi {
+			break
+		}
+	}
+	return list
+}
