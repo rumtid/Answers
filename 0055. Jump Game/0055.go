1986c7bce965efd04905003d68a14f3d5ca6ebf7
rumtid <rumtid@users.noreply.github.com>
2020-03-03 21:16:55 +0800

    Answer Q55 & Q56

---
+++ 0055. Jump Game/0055.go
@@ -0,0 +1,14 @@
+package main
+
+func canJump(nums []int) bool {
+	var j int
+	for i, n := range nums {
+		if i > j {
+			return false
+		}
+		if i+n > j {
+			j = i + n
+		}
+	}
+	return true
+}
---
+++ 0055. Jump Game/0055_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func Example_case1() {
+	ans := canJump([]int{2, 3, 1, 1, 4})
+	fmt.Print(ans)
+	// Output:
+	// true
+}
+
+func Example_case2() {
+	ans := canJump([]int{3, 2, 1, 0, 4})
+	fmt.Print(ans)
+	// Output:
+	// false
+}
