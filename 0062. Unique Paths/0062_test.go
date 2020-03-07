ab1ad52522409855d41e8de771afe37b4254be55
rumtid <rumtid@users.noreply.github.com>
2020-03-07 19:29:25 +0800

    Answer Q61 & Q62

---
+++ 0062. Unique Paths/0062_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func Example_case1() {
+	ans := uniquePaths(3, 2)
+	fmt.Print(ans)
+	// Output:
+	// 3
+}
+
+func Example_case2() {
+	ans := uniquePaths(7, 3)
+	fmt.Print(ans)
+	// Output:
+	// 28
+}
