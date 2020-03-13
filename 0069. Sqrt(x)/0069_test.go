9e497ccd9e2d90bf62976439bba304a3621c4e7b
rumtid <rumtid@users.noreply.github.com>
2020-03-13 18:32:32 +0800

    Answer Q69 & Q70

---
+++ 0069. Sqrt(x)/0069_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func Example_case1() {
+	ans := mySqrt(4)
+	fmt.Print(ans)
+	// Output:
+	// 2
+}
+
+func Example_case2() {
+	ans := mySqrt(8)
+	fmt.Print(ans)
+	// Output:
+	// 2
+}
---
+++ 0070. Climbing Stairs/0070_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func Example_case1() {
+	ans := climbStairs(2)
+	fmt.Print(ans)
+	// Output:
+	// 2
+}
+
+func Example_case2() {
+	ans := climbStairs(3)
+	fmt.Print(ans)
+	// Output:
+	// 3
+}
