febd0b4a465f44d5517436e5e19976547defc7e4
rumtid <rumtid@users.noreply.github.com>
2020-03-17 20:19:15 +0800

    Answer Q73 & Q74

---
+++ 0074. Search a 2D Matrix/0074_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func Example_case1() {
+	ans := searchMatrix([][]int{
+		[]int{1, 3, 5, 7},
+		[]int{10, 11, 16, 20},
+		[]int{23, 30, 34, 50},
+	}, 3)
+	fmt.Print(ans)
+	// Output:
+	// true
+}
+
+func Example_case2() {
+	ans := searchMatrix([][]int{
+		[]int{1, 3, 5, 7},
+		[]int{10, 11, 16, 20},
+		[]int{23, 30, 34, 50},
+	}, 13)
+	fmt.Print(ans)
+	// Output:
+	// false
+}
