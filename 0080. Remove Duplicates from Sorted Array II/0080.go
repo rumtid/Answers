31d4fe18495bebcde2064e90334c6135259f95d5
rumtid <rumtid@users.noreply.github.com>
2020-03-21 21:31:11 +0800

    Answer Q79 & Q80

---
+++ 0080. Remove Duplicates from Sorted Array II/0080.go
@@ -0,0 +1,11 @@
+package main
+
+func removeDuplicates(nums []int) int {
+	i, j := 2, 2
+	for ; j < len(nums); j++ {
+		if nums[j] != nums[i-2] {
+			nums[i], i = nums[j], i+1
+		}
+	}
+	return i
+}
