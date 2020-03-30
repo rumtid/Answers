7fbca3f7c940d8608c0778d01538e987ba5b4859
rumtid <rumtid@users.noreply.github.com>
2020-03-30 19:26:27 +0800

    Answer Q89 & Q90

--- 0090. Subsets II/0090.go
+++ 0090. Subsets II/0090.go
@@ -1,9 +1,12 @@
 package main
 
+import "sort"
+
 var set []int
 var sets [][]int
 
-func subsets(nums []int) [][]int {
+func subsetsWithDup(nums []int) [][]int {
+	sort.Ints(nums)
 	sets = [][]int{}
 	for i := 0; i <= len(nums); i++ {
 		search(nums, i)
@@ -17,6 +20,9 @@ func search(nums []int, n int) {
 		return
 	}
 	for i := 0; i < len(nums)-n+1; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
 		set = append(set, nums[i])
 		search(nums[i+1:], n-1)
 		set = set[:len(set)-1]
